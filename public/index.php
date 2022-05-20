cbb0857d80100ef0232d8e79bfe689c96f3dfb13
boulanouar-amine <boulanouarmohamedamine@gmail.com>
2022-05-20 13:12:29 -0700

    first sidebar integration

--- public/index.php
+++ public/index.php
@@ -5,5 +5,10 @@ require_login();
 ?>
 
 <?php view('header', ['title' => 'Dashboard']) ?>
+
+<?php view('sidebar') ?>
 <p>Welcome <?= current_user() ?> <a href="logout.php">Logout</a></p>
+
+
+
 <?php view('footer') ?>
