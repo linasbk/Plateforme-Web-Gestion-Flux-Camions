e8c011c4c0b89dbeff0058f58173c75f1b445b78
boulanouar-amine <boulanouarmohamedamine@gmail.com>
2022-05-21 05:24:38 -0700

    approuver

--- public/admin.php
+++ public/admin.php
@@ -9,51 +9,56 @@ require __DIR__ . '/../src/approve.php';
 
 <p>hello admin</p>
 
-<div class="contenue">
+<div class="main">
+    <?php view('sidebar') ?>
+    <div class="background">
+        <div class="contenue">
 
-    <titre>
-        <strong>Gérer les utilisateurs</strong>
-    </titre>
-
-
-    <table>
-
-        <tr>
-            <th>Id</th>
-            <th>Nom d'utilisateur</th>
-            <th>E-mail</th>
-            <th>Action</th>
-
-        </tr>
-
-        <?php
-        $users = find_unnapproved_users();
-        foreach ($users as $user) {
-        ?>
-
-            <tr>
-
-                <td><?php echo $user['id']; ?></td>
-                <td><?php echo  $user['username']; ?></td>
-                <td><?php echo  $user['email']; ?></td>
-
-
-                <td>
-
-                    <form method="post" class="modifier">
-                        <button id="submit" name="submit">approuver</button>
-                        <input type="hidden" name="id" value="<?php echo $user['id']; ?>">
-                    </form>
-
-                </td>
+            <titre>
+                <strong>Gérer les utilisateurs</strong>
+            </titre>
 
 
 
-            </tr>
-    </table>
+            <table>
 
-<?php } ?>
+                <tr>
+                    <th>Id</th>
+                    <th>Nom d'utilisateur</th>
+                    <th>E-mail</th>
+                    <th>Action</th>
+
+                </tr>
+
+                <?php
+                $users = find_unnapproved_users();
+                foreach ($users as $user) {
+                ?>
+
+                    <tr>
+
+                        <td><?php echo $user['id']; ?></td>
+                        <td><?php echo  $user['username']; ?></td>
+                        <td><?php echo  $user['email']; ?></td>
+
+                        <td>
+
+                            <form method="post" class="modifier">
+                                <button id="submit" name="submit">Approuver</button>
+                                <input type="hidden" name="id" value="<?php echo $user['id']; ?>">
+                            </form>
+
+                        </td>
+
+                    </tr>
+                <?php } ?>
+            </table>
+
+
+
+        </div>
+
+
+        <?php view('footer') ?>
+    </div>
 </div>
-
-
-<?php view('footer') ?>
--- src/login.php
+++ src/login.php
@@ -39,7 +39,6 @@ if (is_post_request()) {
 
     if (is_user_admin() && is_user_approved()) redirect_to('admin.php');
 
-
     if (is_user_approved()) redirect_to('index.php');
 } else if (is_get_request()) {
     [$errors, $inputs] = session_flash('errors', 'inputs');
