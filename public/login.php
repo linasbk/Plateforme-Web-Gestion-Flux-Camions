24fac7d00141dbcddb06af3e977abc384723d6f0
boulanouar-amine <86970749+boulanouar-amine@users.noreply.github.com>
2022-05-25 10:28:29 +0100

    style for login pages

--- public/login.php
+++ public/login.php
@@ -4,40 +4,58 @@ require __DIR__ . '/../src/bootstrap.php';
 require __DIR__ . '/../src/login.php';
 ?>
 
-<?php view('header', ['title' => "Se connecter", 'css' => 'auth']) ?>
+<?php view('header', ['title' => "Se connecter", 'css' => 'register']) ?>
 
-<?php if (isset($errors['login'])) : ?>
-    <div class="alert alert-error">
-        <?= $errors['login'] ?>
-    </div>
-<?php endif ?>
 
-<form action="login.php" method="post">
-    <h1>Se connecter</h1>
-    <div>
-        <label for="username">Nom d'utilisateur:</label>
-        <input type="text" name="username" id="username" value="<?= $inputs['username'] ?? '' ?>">
-        <small><?= $errors['username'] ?? '' ?></small>
-    </div>
 
-    <div>
-        <label for="password">Mot de passe:</label>
-        <input type="password" name="password" id="password">
-        <small><?= $errors['password'] ?? '' ?></small>
-    </div>
+<body class="indexbody">
 
-    <div>
-        <label for="remember_me">
-            <input type="checkbox" name="remember_me" id="remember_me" value="checked" <?= $inputs['remember_me'] ?? '' ?> />
-            Se souvenir de moi
-        </label>
-        <small><?= $errors['agree'] ?? '' ?></small>
-    </div>
-    <section>
-        <button type="submit">Se connecter</button>
-        <a href="register.php">Inscription</a>
-    </section>
-    <small><?= $errors['approved'] ?? '' ?></small>
-</form>
+    <div class="login-wrapper">
+        <?php if (isset($errors['login'])) : ?>
+            <div class="alert alert-error">
+                <?= $errors['login'] ?>
+            </div>
+        <?php endif ?>
 
-<?php view('footer') ?>
+        <div action="" class="form-contenue">
+
+            <h1 style="text-align:center;">Se connecter</h1>
+            <div class="card-body" id="formContainer">
+
+                <div class="Form" id="loginForm">
+
+
+                    <form action="login.php" method="post">
+
+                        <div class="input-group">
+
+                            <input type="text" name="username" id="username" value="<?= $inputs['username'] ?? '' ?>" required>
+                            <label for="username">Nom d'utilisateur:</label>
+                            <small><?= $errors['username'] ?? '' ?></small>
+                        </div>
+
+                        <div class="input-group">
+
+                            <input type="password" name="password" id="password" required>
+                            <label for="password">Mot de passe:</label>
+                            <small><?= $errors['password'] ?? '' ?></small>
+                        </div>
+
+                        <div style="display: flex;">
+                            <label for="remember_me">
+                                <input type="checkbox" name="remember_me" id="remember_me" value="checked" <?= $inputs['remember_me'] ?? '' ?> />
+                                Se souvenir de moi
+                            </label>
+                            <small><?= $errors['agree'] ?? '' ?></small>
+                        </div>
+                        <section>
+                            <button type="submit" class="submit-btn">Se connecter</button>
+                            <footer class="insfooter">Pas un membre! <a href="register.php">Inscription</a></footer>
+                        </section>
+                        <small><?= $errors['approved'] ?? '' ?></small>
+                    </form>
+
+
+                </div>
+            </div>
+        </div>
--- public/register.php
+++ public/register.php
@@ -3,53 +3,52 @@ require __DIR__ . '/../src/bootstrap.php';
 require __DIR__ . '/../src/register.php';
 ?>
 
-<?php view('header', ['title' => 'Inscription', 'css' => 'auth']) ?>
+<?php view('header', ['title' => 'Inscription', 'css' => 'register']) ?>
 
-<form action="register.php" method="post">
-    <h1>Inscription</h1>
 
-    <div>
-        <label for="username">Nom d'utilisateur:</label>
-        <input type="text" name="username" id="username" value="<?= $inputs['username'] ?? '' ?>" class="<?= error_class($errors, 'username') ?>">
-        <small><?= $errors['username'] ?? '' ?></small>
-    </div>
+<body class="indexbody">
 
-    <div>
-        <label for="email">Email:</label>
-        <input type="email" name="email" id="email" value="<?= $inputs['email'] ?? '' ?>" class="<?= error_class($errors, 'email') ?>">
-        <small><?= $errors['email'] ?? '' ?></small>
-    </div>
+    <div class="login-wrapper">
+        <div action="" class="form-contenue">
+            <h1 style="text-align:center;">Inscription</h1>
+            <div class="card-body" id="formContainer">
 
-    <div>
-        <label for="password">Mot de passe:</label>
-        <input type="password" name="password" id="password" value="<?= $inputs['password'] ?? '' ?>" class="<?= error_class($errors, 'password') ?>">
-        <small><?= $errors['password'] ?? '' ?></small>
-    </div>
+                <div class="Form" id="loginForm">
 
-    <div>
-        <label for="password2">Confirmez le mot de passe:</label>
-        <input type="password" name="password2" id="password2" value="<?= $inputs['password2'] ?? '' ?>" class="<?= error_class($errors, 'password2') ?>">
-        <small><?= $errors['password2'] ?? '' ?></small>
-    </div>
+                    <form action="register.php" method="post">
 
-    <!--
-        <div>
-        <label for="agree">
-            <input type="checkbox" name="agree" id="agree" value="checked" <? #= $inputs['agree'] ?? '' 
-                                                                            ?> /> I
-            agree
-            with the
-            <a href="#" title="term of services">term of services</a>
-        </label>
-        <small><? #= $errors['agree'] ?? '' 
-                ?></small>
-    </div>
--->
 
-    <button type="submit">Inscription</button>
+                        <div class="input-group">
+                            <input type="text" name="username" id="username" value="<?= $inputs['username'] ?? '' ?>" class="<?= error_class($errors, 'username') ?>" required>
+                            <label for="username">Nom d'utilisateur</label>
+                            <small><?= $errors['username'] ?? '' ?></small>
+                        </div>
 
-    <footer>Déjà membre? <a href="login.php">Se connecter</a></footer>
+                        <div class="input-group">
+                            <input type="email" name="email" id="email" value="<?= $inputs['email'] ?? '' ?>" class="<?= error_class($errors, 'email') ?>" required>
+                            <label for="email">Email</label>
+                            <small><?= $errors['email'] ?? '' ?></small>
+                        </div>
 
-</form>
+                        <div class="input-group">
+                            <input type="password" name="password" id="password" value="<?= $inputs['password'] ?? '' ?>" class="<?= error_class($errors, 'password') ?>" required>
+                            <label for="password">Mot de passe</label>
+                            <small><?= $errors['password'] ?? '' ?></small>
+                        </div>
 
-<?php view('footer') ?>
+                        <div class="input-group">
+                            <input type="password" name="password2" id="password2" value="<?= $inputs['password2'] ?? '' ?>" class="<?= error_class($errors, 'password2') ?>" required>
+                            <label for="password2">Confirmez le mot de passe</label>
+                            <small><?= $errors['password2'] ?? '' ?></small>
+                        </div>
+
+
+                        <button type="submit" class="submit-btn">Inscription</button>
+
+                        <footer class="insfooter">Déjà membre? <a href="login.php">Se connecter</a></footer>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+        <?php view('footer') ?>
