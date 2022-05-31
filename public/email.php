ee55dc94fac49a9ed304b0ed293f7dfc2f10ed6f
boulanouar-amine <86970749+boulanouar-amine@users.noreply.github.com>
2022-05-31 12:30:19 +0100

    page for activation

---
+++ public/email.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<style type="text/css">
+    body {
+        background-color: #88BDBF;
+        margin: 0px;
+    }
+</style>
+
+<body>
+    <table border="0" width="50%" style="margin:auto;padding:30px;background-color: #F3F3F3;border:1px solid #FF7A5A;">
+
+        <tr>
+            <td>
+                <table border="0" cellpadding="0" cellspacing="0" style="text-align:center;width:100%;background-color: #fff;">
+                    <tr>
+                        <td style="background-color:#ff6b00;height:100px;font-size:50px;color:#fff;">
+                            <table border="0" width="100%">
+                                <tr>
+                                    <td>
+                                        <img src="https://i.imgur.com/IAYN14n.png" style="padding-top:20px" alt="" width="100px" height="90px">
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+
+                    </tr>
+                    <tr>
+                        <td>
+                            <h1 style="padding-top:25px;">Bonjour!</h1>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p style="padding:0px 100px;">
+                                Veuillez cliquer sur le bouton pour activer votre compte :
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+
+                            <form method="get" action="here">
+
+                                <button type="submit" style="margin:10px 0px 30px 0px;border-radius:4px;padding:10px 20px;border: 0;color:#fff;background-color:#ff6b00;">Verify Email address</button>
+                            </form>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Si cela ne fonctionne pas, veuillez copiez et collez le lien suivant dans votre navigateur :
+                        </td>
+                        <a href="here">here</a>
+                    </tr>
+
+                </table>
+            </td>
+        </tr>
+        <tr>
+            <td>
+
+</body>
+
+</html>
--- src/auth.php
+++ src/auth.php
@@ -288,12 +288,78 @@ function send_activation_email(string $email, string $activation_code): void
     $activation_link = APP_URL . "/activate.php?email=$email&activation_code=$activation_code";
 
     // set email subject & body
-    $subject = ' Veuillez activer votre compte';
-    $message = <<<MESSAGE
-            Bonjour,
-            Veuillez cliquer sur le lien suivant pour activer votre compte :
-            $activation_link
-            MESSAGE;
+    $subject = 'Veuillez activer votre compte';
+    $message = '<!DOCTYPE html>
+    <html>
+    
+    <head>
+        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <style type="text/css">
+        body {
+            background-color: #88BDBF;
+            margin: 0px;
+        }
+    </style>
+    
+    <body>
+        <table border="0" width="50%" style="margin:auto;padding:30px;background-color: #F3F3F3;border:1px solid #FF7A5A;">
+    
+            <tr>
+                <td>
+                    <table border="0" cellpadding="0" cellspacing="0" style="text-align:center;width:100%;background-color: #fff;">
+                        <tr>
+                            <td style="background-color:#ff6b00;height:100px;font-size:50px;color:#fff;">
+                                <table border="0" width="100%">
+                                    <tr>
+                                        <td>
+                                            <img src="https://i.imgur.com/IAYN14n.png" style="padding-top:20px" alt="" width="100px" height="90px">
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+    
+                        </tr>
+                        <tr>
+                            <td>
+                                <h1 style="padding-top:25px;">Bonjour!</h1>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p style="color:black;font:size:16px padding:0px 100px; padding-bottom:10px;">
+                                    Veuillez cliquer sur le bouton pour activer votre compte :
+                                </p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                            <a  href="' . $activation_link . '" style="margin:10px 0px 30px 0px;border-radius:4px;  text-decoration: none; padding:10px 20px;border: 0;color:#fff;background-color:#ff6b00;">Vérifier votre email</a>
+                            </td>
+                        </tr>
+                        <br>
+                       
+                        <tr>
+                            <td>
+                            <pre style="color:black;font:size:16px padding-bottom:10px;">
+Si cela ne fonctionne pas, veuillez copiez et 
+collez le lien suivant dans votre navigateur :</pre>
+
+                                <a href="' . $activation_link . '">' . $activation_link . '</a>
+                            </td>
+                         
+                        </tr>
+    
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td>
+    
+    </body>
+    
+    </html>';
+
     // email header
     $header = "From: " . "innovatelsup@gmail.com";
 
